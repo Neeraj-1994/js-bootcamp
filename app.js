9b4791cd6ff04fd2379f7d73d066da480c53e39e
Neeraj Pradhan <neeraj.pradhan@selise.ch>
2021-03-03 11:51:27 +0600

    else practice

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
-if (1 !== 1){
-    console.log("It's True!");
+// Performance Review
+// 3 - Superstar
+// 2 - meets expectations
+// 1 - needs improvement
+// anything else - wtf?
+let rating = 2;
+
+if (rating === 3) {
+    console.log('YOU ARE A SUPERSTAR!');
+} 
+else if (rating === 2) {
+    console.log('MEETS EXPECTATIONS');
+}
+else if (rating === 1) {
+    console.log('NEEDS IMPROVEMENT');
+}
+else {
+    console.log('INVALID RATING!');
 }
 
 // Example 2
 
-let rating = 3;
+let highScore = 1430;
+let userScore = 1200;
 
-if(rating === 3){
-    console.log('YOU ARE A SUPERSTAR!')
+if(userScore >= highScore) {
+    console.log(`Congrats, you have the new 
+    high score of ${userScore}`);
+    highScore = userScore;
 }
-
-// Example 3
-let num = 37;
-
-if (num % 2 !== 0){
-    console.log("ODD NUMBER!");
+else {
+    console.log(
+        `Good Game. Your score of ${userScore} did not 
+        beat the high score of ${highScore}`
+    )
 }
