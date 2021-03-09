c80fcc057392ff72b2a4712147e40bd4c0a0100e
Neeraj Pradhan <neeraj.pradhan@selise.ch>
2021-03-09 10:56:54 +0600

    completed nesting conditionals

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-if (1 !== 1){
-    console.log("It's True!");
-}
+let password = 'kitty';
 
-// Example 2
-
-let rating = 3;
-
-if(rating === 3){
-    console.log('YOU ARE A SUPERSTAR!')
-}
-
-// Example 3
-let num = 37;
-
-if (num % 2 !== 0){
-    console.log("ODD NUMBER!");
+if (password.length >= 6) {
+    if (password.indexOf(' ') === -1) {
+        console.log("Valid password!");
+    } else {
+        console.log("Password is long enough, but cannot contain spaces!");
+    }
+} else {
+    console.log("Password must be longer!");
 }
