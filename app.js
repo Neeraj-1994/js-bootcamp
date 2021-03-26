30cf0bdce4504c94df7754857b4b43fcfcc3264c
Neeraj Pradhan <neeraj.pradhan@selise.ch>
2021-03-26 12:35:14 +0600

    completed array indices

---
+++ app.js
@@ -0,0 +1,8 @@
+let colors = ['red', 'orange', 'yellow', 'green', 
+'blue', 'indigo', 'violet'];
+
+console.log(colors.length); // 7 Length of the array
+console.log(colors[0]); // "red" To access the first item
+console.log(colors[1]); // "orange" To access the second item
+console.log(colors[7]); // undefined because there is no 8th item, we are accessing something which is not there
+console.log(colors[colors.length - 1]); // "violet" To access the last item
