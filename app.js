f8fcc50bf643d4abc4248ccb033c28726e52ad99
neeraj-selise <neeraj.pradhan@selise.ch>
2021-06-01 18:44:10 +0600

    addingAndUpdatingProperties completed

---
+++ app.js
@@ -0,0 +1,20 @@
+ 
+ // Adding and Updating Properties Practice
+ 
+ const userReviews = {};
+
+ userReviews['queenBee49'] = 4.0;
+
+ console.log(userReviews);
+
+ userReviews.mrSmith78 = 3.5;
+
+ console.log(userReviews);
+
+ userReviews['queenBee49'] += 2;
+
+ console.log(userReviews);
+
+ userReviews.mrSmith78++;
+
+ console.log(userReviews);
